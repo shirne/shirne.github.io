5e0729a645bb72a987df903d877eb2da13ee5ce8
shirne <shirne@126.com>
2022-08-23 07:46:12 +0800

    update dialog

--- dialog/flutter_service_worker.js
+++ dialog/flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b1056c7f6b9dec923cdf740aee2746d1",
-"/": "b1056c7f6b9dec923cdf740aee2746d1",
-"main.dart.js": "2766a54c0862dc73007c8cb98fb15cff",
+"index.html": "8d755f7703dc60551636dc5171a91bbf",
+"/": "8d755f7703dc60551636dc5171a91bbf",
+"main.dart.js": "757ca5e0123685c7e468dcd3fcf5fb45",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
